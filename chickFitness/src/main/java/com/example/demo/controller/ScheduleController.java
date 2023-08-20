5eb6e9cac4a502f1a835d73b861cebd02a21b5a2
annis422 <annis.c422@gmail.com>
2023-08-20 23:43:05 +0800

    更新Controller功能

--- chickFitness/src/main/java/com/example/demo/controller/ScheduleController.java
+++ chickFitness/src/main/java/com/example/demo/controller/ScheduleController.java
@@ -1,41 +1,34 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.example.demo.model.Schedule;
 import com.example.demo.model.ScheduleRepository;
 
 
 @RestController
-@RequestMapping("/schedule") // 網址http://localhost:8080/schedule
+@RequestMapping("/schedule") //網址 http://localhost:8080/schedule
 public class ScheduleController {
+	
 	@Autowired
 	ScheduleRepository sReps;
-
-	
 	
 //取得資料
 	//Json格式
-	//http://localhost:8080/schedule/getAlljson
-	@GetMapping(value = "/getAlljson")
+	//http://localhost:8080/schedule/get/all
+	@GetMapping(value = "/get/all")
 	public List<Schedule> getAllSchedule() {
 		List<Schedule> data = sReps.findAll();
 		return data;
 	}
 	//View
-	//http://localhost:8080/schedule/view/All
-	@GetMapping(value = "/view/All")
+	//http://localhost:8080/schedule/view/all
+	@GetMapping(value = "/view/all")
 	public ModelAndView getAllScheduleView() {
 		List<Schedule> data = null;
 		data = sReps.findAll(); //到此data已被讀取為Json
@@ -43,12 +36,45 @@ public class ScheduleController {
 		view.addObject("fc_data_1", data);
 		return view;
 	}
-//待處理:以redate查詢→ SELECT * FROM workout.schedule WHERE redate = '2023-07-01' ORDER BY schedule_name ASC;
-//待處理:以schedule_name查詢並以sort排序→ SELECT * FROM workout.schedule WHERE schedule_name = 'AAA' ORDER BY sort ASC;
-//	@GetMapping("/view/redate/{url_redate}") //{urlAge}，程式內如需要使用，需以@PathVariable("urlAge") 定義(本處定義型態為Integer 參數名稱age)
-//	public List<Schedule> getRedate(@PathVariable("url_redate") Date redate) {
-//		List<Schedule> data = (sReps.findAll()).stream().filter(u->u.getRedate().equals(redate)).collect(Collectors.toList());
-//		return data;//
-//	}
+	
+	//以會員列出所有排程
+	//http://localhost:8080/schedule/view/{mid}
+	@GetMapping("/get/{mid}")
+	public List<Schedule> getMidObySname(@PathVariable("mid") Integer mid, String sname) {
+		List<Schedule> data = sReps.findByMidOrderBySname(mid);
+		return data;
+	}
+	@GetMapping("/view/{mid}")
+	public ModelAndView viewMidObySname(@PathVariable("mid") Integer mid, String sname) {
+		List<Schedule> data = null;
+		data = sReps.findByMidOrderBySname(mid);
+		ModelAndView view = new ModelAndView("/schedule/viewSchedule");
+		view.addObject("fc_data_1", data);
+		return view;
+	}
+	
+	
+	//以會員的排程名稱做篩選並照sort排序
+	//http://localhost:8080/schedule/view/{mid}/{sname}
+	@GetMapping("/get/{mid}/{sname}")
+	public List<Schedule> getMidSnameObySort(@PathVariable Integer mid, @PathVariable("sname") String sname) {
+		List<Schedule> data = sReps.findByMidAndSnameOrderBySort(mid, sname);
+		return data;
+	}
+	@GetMapping("/view/{mid}/{sname}")
+	public ModelAndView viewMidSnameObySort(@PathVariable Integer mid, @PathVariable("sname") String sname) {
+		List<Schedule> data = null;
+		data = sReps.findByMidAndSnameOrderBySort(mid, sname);
+		ModelAndView view = new ModelAndView("/schedule/viewSchedule");
+		view.addObject("fc_data_1", data);
+		return view;
+	}
+	
+	
+//待處理:
+//以redate查詢???
+//	→ SELECT * FROM workout.schedule WHERE redate = '2023-07-01' ORDER BY schedule_name ASC;
+//	→ Date資料處理與讀取?????
+//mid改對應到username??
 
 }
--- chickFitness/src/main/java/com/example/demo/model/ScheduleRepository.java
+++ chickFitness/src/main/java/com/example/demo/model/ScheduleRepository.java
@@ -1,7 +1,15 @@
 package com.example.demo.model;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
+	// 使用findBy自動化命名規則進行條件搜尋 //條件::member_id=? AND schedule_name=? ORDER BY sort ASC
+	List<Schedule> findByMidAndSnameOrderBySort(Integer mid, String sname);
+	// 使用findBy自動化命名規則進行條件搜尋 //條件:member_id=? ORDER BY schedule_name
+	List<Schedule> findByMidOrderBySname(Integer mid);
+
+	
+	
 
 }
