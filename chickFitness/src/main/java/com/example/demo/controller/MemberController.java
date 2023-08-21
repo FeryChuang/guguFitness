16897358c8f153de31a67647c2f2ad293433bd41
s8700632003 <52704106+s8700632003@users.noreply.github.com>
2023-08-21 16:30:31 +0800

    有寫session方法在登入功能中
    1.註冊功能可以同時新增一筆資料在兩個表格中
    2.登入功能有寫session
    3.最下方為前後端溝通抓session方法

--- chickFitness/src/main/java/com/example/demo/controller/MemberController.java
+++ chickFitness/src/main/java/com/example/demo/controller/MemberController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.model.Chick;
+import com.example.demo.model.ChickRepository;
 import com.example.demo.model.Member;
 import com.example.demo.model.MemberRepository;
 
@@ -16,28 +22,58 @@ import com.example.demo.model.MemberRepository;
 @RestController
 public class MemberController {
 
-	@Autowired
-	MemberRepository dao;
-	
-	@RequestMapping(value = "/membercreate", method = RequestMethod.POST)
-	public ModelAndView add(@RequestBody Member me) {
-		ModelAndView model = new ModelAndView("redirect:/index.html");
-		dao.save(me);
-		return model;
-	}
-	
-	@RequestMapping(value="/index",method=RequestMethod.POST)
-	public ModelAndView signin(@ModelAttribute String userName, String password) {
-		ModelAndView model=null;
-		Boolean b=dao.findByUserNameAndPassword(userName, password);
-		System.out.println(b);
-		if(b) {
-			model=new ModelAndView("signin");
-			model.addObject("username", userName);
-			return model;
-		}else{
-			model=new ModelAndView("redirect:/index.html?info=查無此帳號或密碼錯誤");
-			return model;
-		}	
-	}
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    ChickRepository chickRepository;
+
+    @RequestMapping(value = "/membercreate", method = RequestMethod.POST)
+    public ModelAndView add(@RequestBody Member member) {
+        ModelAndView model = new ModelAndView("redirect:/index.html");
+        
+        // 新增 Member 資料
+        Member savedMember = memberRepository.save(member);
+        
+        // 新增對應的 Chick 資料，設定預設值
+        Chick chick = new Chick();
+        chick.setUser_name(savedMember.getUserName()); // 使用相同的使用者名稱
+        chick.setChick_stage("手無縛雞"); // 設定預設階段
+        chick.setCurrent_level(1); // 設定預設等級
+        chick.setCurrent_exp(0); // 設定預設經驗值
+        chick.setLifecount(0); // 設定預設生命數量
+        chickRepository.save(chick);
+        
+        return model;
+    }
+ ///登入//////////////////////////////////////////////////////   
+    @Autowired
+    public MemberController(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@RequestParam String userName, @RequestParam String password, HttpSession session) {
+        Member member = memberRepository.findByUserNameAndPassword(userName, password);
+
+        if (member != null) {
+            // 将会员信息存储在 HttpSession 中
+            session.setAttribute("loggedInMember", member);
+             System.out.println(session.getAttribute("loggedInMember"));
+            ModelAndView model = new ModelAndView("redirect:/home.html");
+            return model;
+        } else {
+            ModelAndView model = new ModelAndView("redirect:/index.html");
+            model.addObject("info", "Login failed"); // 传递错误信息到登录页面
+            return model;
+        }
+        
+     
+    }
+    @RequestMapping(value="/session",method=RequestMethod.GET)
+    public Member getSession(HttpSession session) {
+   	 return (Member)session.getAttribute("loggedInMember");
+    }
+
+
 }
