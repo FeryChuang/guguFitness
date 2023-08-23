25bbf576ca8c913cefc0d170f0dc128183ad2138
wenshin0726 <134997180+wenshin0726@users.noreply.github.com>
2023-08-23 14:23:21 +0800

    Update ChickRepository
    增加方法

--- chickFitness/src/main/java/com/example/demo/model/ChickRepository.java
+++ chickFitness/src/main/java/com/example/demo/model/ChickRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.model;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChickRepository extends JpaRepository<Chick,Integer>{
+
+	 List<Chick> findByUserName(String userName);
 
 }
